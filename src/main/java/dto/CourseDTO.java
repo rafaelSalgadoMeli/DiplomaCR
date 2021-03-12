e8035e8e68900fc2c91aae7caf73f75ece8b371d
rafaelSalgadoMeli <rafael.salgado@mercadolibre.com.co>
2021-03-12 14:36:14 -0500

    add original project

---
+++ src/main/java/dto/CourseDTO.java
@@ -0,0 +1,23 @@
+package dto;
+
+public class CourseDTO {
+
+    private String name;
+    private Double result;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/dto/DiplomaDTO.java
@@ -0,0 +1,33 @@
+package dto;
+
+public class DiplomaDTO {
+    private String name;
+    private Double average;
+    private String message;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAverage() {
+        return average;
+    }
+
+    public void setAverage(Double average) {
+        this.average = average;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
